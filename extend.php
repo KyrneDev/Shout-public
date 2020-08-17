277a8754d1ccbdd9be1ed044899cc290f7677c64
Charlie K <charlie.kern01@gmail.com>
2020-08-16 23:14:58 -0700

    Marketing

--- extend.php
+++ extend.php
@@ -4,18 +4,21 @@
 namespace Kyrne\Shout;
 
 use Flarum\Extend;
+use Flarum\Formatter\Event\Configuring;
 use Illuminate\Contracts\Events\Dispatcher;
 use Kyrne\Shout\Api\Controllers;
+use Kyrne\ExtCore\Extend\AddKyrneCore;
 
 return [
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/resources/js/admin.js'),
+        ->js(__DIR__.'/js/dist/admin.js'),
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/resources/js/forum.js')
+        ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/extension.less')
         ->route('/messages/{id}', 'messages')
         ->route('/conversations', 'conversation'),
     new Extend\Locales(__DIR__.'/resources/locale'),
+    new AddKyrneCore(),
     (new Extend\Routes('api'))
         ->get('/conversations', 'conversations.index', Controllers\ListConversationsController::class)
         ->get('/messages/{id}', 'messages.list', Controllers\ListMessagesController::class)
@@ -25,14 +28,12 @@ return [
         ->patch('/messages/{id}', 'messages.ecnryption.update', Controllers\UpdateMessageEncryptionController::class)
         ->delete('/messages{id}', 'messages.delete', Controllers\DeleteMessageController::class)
         //->patch('/messages/{id}', 'messages.update', Controllers\UpdateMessageController::class)
-        ->delete('/messages/{id}', 'messages.delete', Controllers\DeleteMessageController::class)
+        //->delete('/messages/{id}', 'messages.delete', Controllers\DeleteMessageController::class)
         ->get('/conversations/{id}', 'conversations.show', Controllers\ShowConversationController::class)
         ->post('/verifyPassword', 'password.verify', Controllers\VerifyPasswordController::class)
         ->post('/encryption', 'keys.populate', Controllers\SaveEncryptionKeysController::class)
         ->get('/encryption/{id}', 'keys.get', Controllers\GetIdentityController::class),
     function (Dispatcher $events) {
         $events->subscribe(Listeners\AddRelationships::class);
-
-        //$events->subscribe(Access\MessagePolicy::class);
     },
 ];
